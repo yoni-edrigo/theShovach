4bc64212802b24a5bd85a7841593453d2ac45aed
yoni vasilevski <info@edrigo.com>
2025-06-21 13:08:53 +0300

    fixed scroll to top with router

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  scrollRestoration: true,
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
